aaef74b650e2e46a5f762a9c9605ef638f1772ea
hajimeong <ha_01@naver.com>
2025-12-07 16:37:19 +0900

    페이징 처리 컨트롤러 구현

--- src/main/java/com/study/Board/controller/BoardController.java
+++ src/main/java/com/study/Board/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.study.Board.controller;
 import com.study.Board.dto.BoardDTO;
 import com.study.Board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -70,5 +73,20 @@ public class BoardController {
     public String delete(@PathVariable Long id){
         boardService.delete(id);
         return "redirect:/board/";
+    }
+
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page=1)Pageable pageable, Model model){
+        Page<BoardDTO> boardList=boardService.paging(pageable);
+
+        int blockLimit=3;
+        int startPage=(((int)(Math.ceil((double)pageable.getPageNumber()/blockLimit)))-1)*blockLimit+1;
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
+
     }
 }
